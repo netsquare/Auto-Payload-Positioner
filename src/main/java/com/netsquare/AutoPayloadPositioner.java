960c5dbec05b7f005b1eb28b331f4b9ccd90d51d
zinjacoder <jafarpathan9090@gmail.com>
2025-04-25 23:44:56 +0530

    prepared context menu class

--- src/main/java/com/netsquare/AutoPayloadPositioner.java
+++ src/main/java/com/netsquare/AutoPayloadPositioner.java
@@ -3,6 +3,11 @@ package com.netsquare;
 
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
+import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
+
+import java.awt.*;
+import java.util.List;
 
 public class AutoPayloadPositioner implements BurpExtension {
     private MontoyaApi api;
@@ -10,8 +15,11 @@ public class AutoPayloadPositioner implements BurpExtension {
     @Override
     public void initialize(MontoyaApi api) {
         this.api = api;
-        // Initialize Extension Properties like name
+        // Initialize Extension name
         api.extension().setName("Auto Payload Positioner");
+        // Register Context Menu Items Provider to user interface
+        api.userInterface().registerContextMenuItemsProvider(new PayloadPositionMenuProvider());
         api.logging().logToOutput("Simple Payload Positioner loaded Successfully!");
     }
+
 }
